5fc3621877fb95167bb7454f54bcaa525c677d43
silmarildust <malin.janna.vega@gmail.com>
2026-07-17 14:47:15 -0400

    Added extra config files

--- Work/Driver/copy_sim_QG.sh
+++ Work/Driver/copy_sim_QG.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-RUNID_START=6  # Start value for runid (trained model num)
-RUNID_END=10  # End value for runid
+RUNID_START=11  # Start value for runid (trained model num)
+RUNID_END=13  # End value for runid
 
 BASE_DIR="/home/mjmvega/Codes/QG/Work/"  # Where to copy code from
 RESULTS_ROOT="/gdata/Results/mjmvega/QG/" #Results directory
